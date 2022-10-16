5e54e7960d7a919528b3b69704a75e2f39870336
pc <rahimjangi1982@gmail.com>
2022-10-16 11:28:05 +0200

    Uploading files done!

--- UploadingFiles/src/main/java/com/raiseup/controller/FileUploadController.java
+++ UploadingFiles/src/main/java/com/raiseup/controller/FileUploadController.java
@@ -1,12 +1,18 @@
 package com.raiseup.controller;
 
+import com.raiseup.exception.StorageFileNotFoundException;
 import com.raiseup.storage.StorageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.stream.Collectors;
 
@@ -18,15 +24,40 @@ public class FileUploadController {
 
     @GetMapping("/")
     public String listUploadedFiles(Model model) {
-        var files=storageService.loadAll().map(
+        var files = storageService.loadAll().map(
                         path -> MvcUriComponentsBuilder.fromMethodName(
                                 FileUploadController.class,
                                 "serveFile",
                                 path.getFileName().toString()).build().toUri().toString())
                 .collect(Collectors.toList());
 
-        model.addAttribute("files",files);
-      log.info("LoadAllFiles: {}",files);
+        model.addAttribute("files", files);
+        log.info("LoadAllFiles: {}", files);
         return "uploadForm";
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+    @PostMapping("/")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/";
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
     }
 }
--- UploadingFiles/src/main/java/com/raiseup/exception/StorageFileNotFoundException.java
+++ UploadingFiles/src/main/java/com/raiseup/exception/StorageFileNotFoundException.java
@@ -1,4 +1,11 @@
 package com.raiseup.exception;
 
-public class StorageFileNotFoundException {
+public class StorageFileNotFoundException extends RuntimeException {
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
